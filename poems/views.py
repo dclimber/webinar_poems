0d0ecadc1730f5239dd3705d8064cbb1169e6f7f
Dastan Abd <dastand.climber@gmail.com>
2020-07-23 23:20:57 +0600

    feat: Add index view

--- poems/views.py
+++ poems/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Poet, Poem
+
+
+def index(request):
+    template_name = 'index.html'
+    poets = list(Poet.objects.all())
+
+    context = {
+        'poets': poets,
+    }
+    return render(request, template_name, context)
