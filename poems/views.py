2393e728bc2cbd8c6d22509754fd00c11f7e137f
Dastan Abd <dastand.climber@gmail.com>
2020-07-24 00:45:30 +0600

    feat: Implement search functionality

--- poems/views.py
+++ poems/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from django.shortcuts import render, get_object_or_404
 
 from .models import Poet, Poem
@@ -43,7 +44,22 @@ def search(request):
     template_name = 'search.html'
     search = False
 
+    query = request.GET.get('q', None)
+    if query is not None:
+        search = True
+
+        poets = list(Poet.objects.filter(
+            Q(first_name__icontains=query) | Q(last_name__icontains=query)
+        ))
+        poems = list(Poem.objects.filter(
+            Q(title__icontains=query) | Q(text__icontains=query)
+        ))
+        results = poets + poems
+    else:
+        results = []
+
     context = {
+        'results': results,
         'search': search,
     }
     return render(request, template_name, context)
