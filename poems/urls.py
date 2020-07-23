28f9ab0eac4e6fb8f8890816f0fd9cf0e96bd128
Dastan Abd <dastand.climber@gmail.com>
2020-07-23 23:30:38 +0600

    feat: Add poet_view

--- poems/urls.py
+++ poems/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("poet/<int:poet_pk>/", views.poet_view, name="poet"),
 ]
--- poems/views.py
+++ poems/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from .models import Poet, Poem
 
@@ -9,5 +9,16 @@ def index(request):
 
     context = {
         'poets': poets,
+    }
+    return render(request, template_name, context)
+
+
+def poet_view(request, poet_pk):
+    template_name = 'poet.html'
+
+    poet = get_object_or_404(Poet, pk=poet_pk)
+
+    context = {
+        'poet': poet,
     }
     return render(request, template_name, context)
