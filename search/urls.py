375dd68927c834563ce47475ab9b97a50048ff60
Dastan Abd <dastand.climber@gmail.com>
2020-08-16 01:02:04 +0600

    feat: Add SearchView and SearchTemplateView

--- search/urls.py
+++ search/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     path("", views.search, name="search"),
+    path("cls/", views.SearchView.as_view(),
+         name="search_view"),
+    path("tpl/", views.SearchTemplateView.as_view(),
+         name="search_template"),
 ]
--- search/views.py
+++ search/views.py
@@ -1,5 +1,6 @@
 from django.db.models import Q
 from django.shortcuts import render
+from django.views.generic import View, TemplateView
 
 from poems.models import Poet, Poem
 
@@ -25,5 +26,61 @@ def search(request):
     context = {
         'results': results,
         'search': search,
+        'query': query,
     }
     return render(request, template_name, context)
+
+
+class SearchView(View):
+
+    def get(self, request):
+        template_name = 'search/search.html'
+        search = False
+
+        query = request.GET.get('q', None)
+        if query is not None:
+            search = True
+
+            poets = list(Poet.objects.filter(
+                Q(first_name__icontains=query) | Q(last_name__icontains=query)
+            ))
+            poems = list(Poem.objects.filter(
+                Q(title__icontains=query) | Q(text__icontains=query)
+            ))
+            results = poets + poems
+        else:
+            results = []
+
+        context = {
+            'results': results,
+            'search': search,
+            'query': query
+        }
+        return render(request, template_name, context)
+
+
+class SearchTemplateView(TemplateView):
+    template_name = 'search/search.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        search = False
+
+        query = self.request.GET.get('q', None)
+        if query is not None:
+            search = True
+
+            poets = list(Poet.objects.filter(
+                Q(first_name__icontains=query) | Q(last_name__icontains=query)
+            ))
+            poems = list(Poem.objects.filter(
+                Q(title__icontains=query) | Q(text__icontains=query)
+            ))
+            results = poets + poems
+        else:
+            results = []
+
+        context['results'] = results
+        context['search'] = search
+        context['query'] = query
+        return context
