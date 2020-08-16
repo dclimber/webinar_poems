668da9e86bf50e487b01fdc27e6417a183b610c8
Dastan Abd <dastand.climber@gmail.com>
2020-08-16 13:40:00 +0600

    test: Add tests for search views

---
+++ search/tests.py
@@ -0,0 +1,109 @@
+from django.test import TestCase, Client
+from django.urls import reverse
+
+from poems.models import Poet, Poem
+
+
+class TestSearch(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.ezekiel_text = (
+            "The path of the righteous man is beset on all sides by the "
+            "iniquities of the selfish and the tyranny of evil men."
+        )
+
+        cls.poet = Poet.objects.create(
+            first_name='Jules',
+            last_name='Winnfield',
+            bio='Mayonnaise',
+            photo_url='https://wiki.tarantino.info/images/Jules.jpg'
+        )
+
+        cls.poem = Poem.objects.create(
+            author=cls.poet,
+            title="Ezekiel 25:17",
+            text=cls.ezekiel_text,
+            year='1994'
+        )
+
+    def setUp(self):
+        self.client = Client()
+
+    def _check_response(self, url, results, search, query):
+        response = self.client.get(
+            url
+        )
+        self.assertEquals(response.status_code, 200)
+        self.assertIn('results', response.context)
+        self.assertIn('search', response.context)
+        self.assertIn('query', response.context)
+        self.assertEquals(response.context['results'], results)
+        self.assertEquals(response.context['search'], search)
+        self.assertEquals(response.context['query'], query)
+
+    def test_search_empty_query(self):
+        urls = (
+            (reverse('search'), 'Search function view'),
+            (reverse('search_view'), 'SearchView'),
+            (reverse('search_template'), 'SearchTemplateView')
+        )
+        for url, sub_description in urls:
+            with self.subTest(sub_description):
+                self._check_response(
+                    url,
+                    [],
+                    False,
+                    None
+                )
+
+    def test_search_valid_query_poet(self):
+        urls = (
+            (reverse('search'), 'Search function view'),
+            (reverse('search_view'), 'SearchView'),
+            (reverse('search_template'), 'SearchTemplateView')
+        )
+        for url, sub_description in urls:
+            with self.subTest(sub_description):
+                query = 'Jul'
+                search_url = f"{url}?q={query}"
+                self._check_response(
+                    search_url,
+                    [self.poet],
+                    True,
+                    query
+                )
+
+    def test_search_valid_query_poem(self):
+        urls = (
+            (reverse('search'), 'Search function view'),
+            (reverse('search_view'), 'SearchView'),
+            (reverse('search_template'), 'SearchTemplateView')
+        )
+        for url, sub_description in urls:
+            with self.subTest(sub_description):
+                query = 'Ezek'
+                search_url = f"{url}?q={query}"
+                self._check_response(
+                    search_url,
+                    [self.poem],
+                    True,
+                    query
+                )
+
+    def test_search_invalid_query(self):
+        urls = (
+            (reverse('search'), 'Search function view'),
+            (reverse('search_view'), 'SearchView'),
+            (reverse('search_template'), 'SearchTemplateView')
+        )
+        for url, sub_description in urls:
+            with self.subTest(sub_description):
+                query = 'Test'
+                search_url = f"{url}?q={query}"
+                self._check_response(
+                    search_url,
+                    [],
+                    True,
+                    query
+                )
